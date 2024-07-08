57ef96c46394d0409fc9ed50a4a26f616355453c
Eros Secundus <erossecundus@gmail.com>
2024-07-08 16:18:18 -0300

    closes #1 - Criado Projeto Angular e configurado o BootStrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'BootCampDevJr_m3c2-Carros';
+  title = 'Car Systems 1.0';
 }
