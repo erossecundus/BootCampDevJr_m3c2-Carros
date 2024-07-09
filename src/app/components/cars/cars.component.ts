ea502fa9dae49e12505deed7ee96b266658ae1f6
Eros Secundus <erossecundus@gmail.com>
2024-07-09 20:17:45 -0300

    closes #7, closes #8 - Botões Salvar, Alterar e Remover

--- src/app/components/cars/cars.component.ts
+++ src/app/components/cars/cars.component.ts
@@ -9,7 +9,9 @@ import { Car } from '../../car';
 export class CarsComponent {
   
   // obj vazio do tipo Car; passa do pai para o filho pelo input
-  car : Car = {} as Car;  
+  car : Car = {} as Car;
+  isUpdate : boolean = false;
+  idCount : number = 4;                       // começa em 4 para os testes
 
   cars : Car[] = [
     {id:1, name:'corsa', assembler:'Chevrolet', price:20000, year:2015},
@@ -17,11 +19,23 @@ export class CarsComponent {
     {id:3, name:'fusca', assembler:'Volkswagen', price:30000, year:1975}
   ];
 
-  // método para salvar novo car no array cars
+  // método para salvar um car no array cars
   saveCar() {
-    this.car.id = this.cars.length + 1;     // determinando proximo id
-    this.cars.push(this.car);               // salvando em memória
-    this.car = {} as Car;                   // Limpando o obj
-  }
+    if(!this.isUpdate) {                      // se não for update:
+      this.car.id = this.idCount;             // determina um id para novo elemento
+      this.idCount++;
+      this.cars.push(this.car);               // salvando em memória o novo elemento
+    }
+    this.car = {} as Car;                     // Limpando o obj
+    this.isUpdate = false;                    // retorna isUpdate para falso toda vez
+  };
 
+  update(updCar : Car) {       // faz a atualização de forma automática no array (two-way binding)
+    this.car = updCar;
+    this.isUpdate = true;
+  };
+
+  remove(remCar : Car) {       // filtra o que for diferente do obj que estiver no input
+    this.cars = this.cars.filter(c => c !== remCar);
+  };
 }
