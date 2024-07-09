43ba8d8d3b25cbedeeae6ec225eb36293d1cf371
Eros Secundus <erossecundus@gmail.com>
2024-07-09 18:42:20 -0300

    closes #6 - Bindings e tratamento de Eventos

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output,} from '@angular/core';
 import { Car } from '../../car';
+import { never } from 'rxjs';
 
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
   styleUrl: './car.component.css'
 })
+
+
 export class CarComponent {
-  car : Car = {
-    id:1, name:'corsa', assembler:'Chevrolet', price:20000, year:2015
-  };
+  
+  // anotação: a info vem de fora (do obj no componente cars)
+  @Input()
+  car : Car = {} as Car;
+
+  // avisa que o método save() vai fazer um Event Emitter
+  @Output()
+  saveEmitter = new EventEmitter;
+  save() {
+    this.saveEmitter.emit();  // vai ser lido no tamplate do componente cars
+  }
 }
--- src/app/components/cars/cars.component.ts
+++ src/app/components/cars/cars.component.ts
@@ -7,9 +7,21 @@ import { Car } from '../../car';
   styleUrl: './cars.component.css'
 })
 export class CarsComponent {
+  
+  // obj vazio do tipo Car; passa do pai para o filho pelo input
+  car : Car = {} as Car;  
+
   cars : Car[] = [
     {id:1, name:'corsa', assembler:'Chevrolet', price:20000, year:2015},
     {id:2, name:'palio', assembler:'Fiat', price:25000, year:2017},
     {id:3, name:'fusca', assembler:'Volkswagen', price:30000, year:1975}
   ];
+
+  // método para salvar novo car no array cars
+  saveCar() {
+    this.car.id = this.cars.length + 1;     // determinando proximo id
+    this.cars.push(this.car);               // salvando em memória
+    this.car = {} as Car;                   // Limpando o obj
+  }
+
 }
